3e1e0913299315a98e05b27013c0ebd53da237a1
lljjssjjll <lljjssjjll@gmail.com>
2021-03-17 16:02:16 +0900

    초기 설정

---
+++ src/test/java/com/lljjssjjll/practice/etc/BatchInsertTests.java
@@ -0,0 +1,13 @@
+package com.lljjssjjll.practice.etc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Batch insert 테스트")
+public class BatchInsertTests {
+
+    @Test
+    void test_01() {
+
+    }
+}
